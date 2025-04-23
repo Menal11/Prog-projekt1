519e478b4f743a6fdfad38031aba1f6dbb175f55
Zaynab <zaynabchaudhry@icloud.com>
2025-04-23 15:08:33 +0200

    Update database.js

--- database.js
+++ database.js
@@ -23,7 +23,8 @@ async function createUser(brugernavn, email, password, repeatpassword) {
       request.input('repeatpassword', sql.VarChar, repeatpassword) //
 
 
-      await request.query(`INSERT INTO eksamen.users (brugernavn, email, password, repeatpassword)
+      await request.query(
+        `INSERT INTO eksamen.users (brugernavn, email, password, repeatpassword)
         VALUES @brugernavn, @email, @password, @repeatpassword`) // disse skal bruges ALLE steder hvor der er brugerinput 
   
     } catch (err) {
@@ -32,3 +33,32 @@ async function createUser(brugernavn, email, password, repeatpassword) {
       await sql.close(); //  !! afbryder endeligt forbindelsen i slutningen af funktionen (finally)
     }
   }
+
+  // funktion "opret konto"
+  async function createAccount(kontoID, brugernavn, navn, valuta, saldo, oprettelses_dato, tilstand, reference_til_bank) {
+    try {
+        await sql.connect(config);
+
+        const oprettelses_dato = new Date();
+        let tilstand = "åben"; // kontoen kan jo lukkes derfor har vi valgt "let"
+        let saldo = 0; // saldo er 0 ved oprettelse, da der er ingen transaktioner endnu
+
+        request.input('brugernavn', sql.VarChar, brugernavn) //
+        request.input('navn', sql.VarChar, navn) //
+        request.input('valuta', sql.VarChar, valuta) //
+        request.input('reference_til_bank', sql.VarChar, reference_til_bank) //
+
+
+        await request.query (
+          `INSERT INTO dbo.accounts (kontoID, brugernavn, navn, valuta, saldo, oprettelses_dato, tilstand, reference_til_bank)
+        VALUES @kontoID, @brugernavn, @navn, @valuta, ${saldo}, ${oprettelses_dato}, ${tilstand}, reference_til_bank
+        `);
+
+        console.log('Konto tilføjet');
+
+    } catch (err) {
+      alert('Fejl ved oprettelse:', err);
+    } finally {
+      await sql.close(); //  !! afbryder endeligt forbindelsen i slutningen af funktionen (finally)
+    }
+}
